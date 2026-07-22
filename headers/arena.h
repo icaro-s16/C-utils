86b63dc95913a66cb5500384b686b3e9e02fdc50
Ikr <santanaikaru@gmail.com>
2026-07-22 10:56:04 -0300

    refac: Refactor arena.h with enums and static structs
    Refactor arena.h to use enums and static structs for better organization.

--- headers/arena.h
+++ headers/arena.h
@@ -5,45 +5,50 @@
 #include <stdio.h>
 #include "vector.h"
 
-#define SINGLE_PAGE 0b0001
+enum{
+    SINGLE_PAGE = 0b0001
+};
 
 typedef unsigned char byte;
 
-
-struct Node{
+typedef struct Node Node;
+static struct Node{
     void* data;
     struct Node* next;
     struct Node* prev;
 };
 
-typedef struct{
+typedef struct ArenaPage ArenaPage;
+static struct ArenaPage{
     size_t offset;
     byte* chunck;
-}ArenaPage;
+};
 
-typedef struct{
+typedef ArenaAllocation;
+static struct ArenaAllocation{
     unsigned current_page;
     size_t size;
     byte* address;
-}ArenaAllocation;
+};
 
-typedef struct{
+typedef struct Arena Arena;
+typedef struct Arena{
     unsigned flags;
     size_t page_size;
     size_t n_pages;
     Vector *allocations; 
-    struct Node* head;
-    struct Node* tail;
-}Arena;
+    Node* head;
+    Node* tail;
+};
 
 Arena* arena_construct(size_t page_size, unsigned flags);
 
 void arena_destroy(Arena* arena);
 
-void arena_push_page(Arena** arena);
+static void arena_push_page(Arena** arena);
 
 void* arena_malloc(Arena* arena, size_t data_size);
 
 void* arena_realloc(Arena* arena, void* address, size_t new_size);
 
-void arena_free_pages(Arena* arena);
+static void arena_free_pages(Arena* arena);
