1a6df112195944e350881a00ac849d91f7a731d7
Ikr <santanaikaru@gmail.com>
2026-07-22 10:17:08 -0300

    refac: Refactor hashmap.h with enums and static functions
    Refactor hashmap.h to use enums for constants and improve struct definitions.

--- headers/hashmap.h
+++ headers/hashmap.h
@@ -6,35 +6,37 @@
 #include <math.h>
 #include <stdio.h>
 
-#define FNV_OFFSET_BASIS_32 2166136261 
+static enum{
+    FNV_OFFSET_BASIS_32 = 2166136261, 
+    FNV_PRIME_32        = 16777619,
+    BASE_BUCKETS_SIZE   = 16
+};
 
-#define FNV_PRIME_32 16777619 
-
-#define BASE_BUCKETS_SIZE 16
-
-typedef struct{
+typedef struct Pair Pair;
+struct Pair{
     size_t key_size;
     size_t data_size;
-    struct Pair* next;
+    Pair* next;
     void* key;
     void* data;
-}Pair;
+};
 
-typedef struct{
+typedef struct HashMap HashMap;
+struct HashMap{
     size_t size;
     size_t capacity;
     Pair *buckets;
-}HashMap;
+};
 
-uint32_t fnv1_32_hash(const void* data, const size_t data_size);
+static uint32_t fnv1_32_hash(const void* data, const size_t data_size);
 
-size_t hash_function(const uint32_t hash_code, const size_t hash_map_size);
+static size_t hash_function(const uint32_t hash_code, const size_t hash_map_size);
 
 HashMap* hashmap_construct();
 
 void hashmap_destroy(HashMap* map);
 
-HashMap* hashmap_rehash(HashMap* map, const size_t new_capacity);
+static HashMap* hashmap_rehash(HashMap* map, const size_t new_capacity);
 
 int hashmap_add(HashMap** map, const void* key, const void* data, const size_t key_size, const size_t data_size);
 
